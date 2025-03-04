8c411becc63ac70643734baf7c8499392ebb4ccf
joeleg96 <joeleg96@gmail.com>
2025-03-04 14:49:29 -0600

    Update index.js
    fixed issue where checkAnswer was failing to properly check user inputs due to userClickedPattern not being reset to an empty array.
    
    added a startOver function to allow the game to reset by setting the gamePattern to an empty array.

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
 var level = 0;
 
+$(document).on("keydown", function(){
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }     
+}); 
+
+$(".btn").on("click", function(event) {
+    var userChosenColor = (event.target.id);
+    userClickedPattern.push(userChosenColor);
+
+    makeSound(userChosenColor);
+    animatePress(userChosenColor); 
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(lastColor) {
+    if (userClickedPattern[lastColor] === gamePattern[lastColor]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout (function() {
+                nextSequence();
+            }, 1000);
+            }
+        } else {
+        var errorSound = new Audio("sounds/wrong.mp3");
+        errorSound.play();
+        $("body").addClass("game-over");
+        setTimeout (function() {
+            $("body").removeClass("game-over")();
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart")
+
+        startOver();
+        }
+};
 
 function nextSequence() { 
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = (Math.floor(Math.random()*4));
     var randomChosenColor = buttonColors[randomNumber];
-    var sound = randomChosenColor;
     gamePattern.push(randomChosenColor);
     randomChosenColorID = ("#" + randomChosenColor);
     $(randomChosenColorID).fadeOut().fadeIn();
 
-    makeSound(sound);
-
-    $("#level-title").html("Level " + level);
-    level = level + 1;   
+    makeSound(randomChosenColor);     
 };
 
-$(document).on("click", function(event) {
-    var userChosenColor = (event.target.id);
-    userClickedPattern.push(userChosenColor);
-    var currentColor = ("#" + userChosenColor);
-    $(currentColor).fadeOut().fadeIn();
-    var sound = userChosenColor;
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout (function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100)
+}
 
-    makeSound(sound); 
-
-});
-
-
-function makeSound(sound) {
-    switch (sound) {
+function makeSound(color) {
+    switch (color) {
         case "green":
             var green = new Audio("sounds/green.mp3");
             green.play();
@@ -57,10 +89,13 @@ function makeSound(sound) {
         break;
 
     }
+} 
+
+
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false; 
 }
-
-
-$(document).one("keydown", function(){
-    nextSequence();   
-});   
 
